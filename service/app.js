be401d98c5b881ad0757b8f5585a3f1f56c72341
front_coder <845882161@qq.com>
2017-08-21 19:20:34 +0800

    开发API

--- service/app.js
+++ service/app.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3001;
 //路由配置
 const routes = require('./Routes/rotue');
-//加载入口页面
+//注册入口
 routes.Home(app);
+//注册API
+routes.API(app);
+
 //设置静态文件
 app.use(express.static('./Public'));
 var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
-    console.log(server.address());
     console.log('listening at http://%s:%s', host, port);
 });
