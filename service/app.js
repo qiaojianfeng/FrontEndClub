55dfff202a0ef51f54ab7024c887ac7e0b62bfdb
front_coder <845882161@qq.com>
2017-08-21 19:00:31 +0800

    输出首页index

--- service/app.js
+++ service/app.js
@@ -1,5 +1,15 @@
-const request = require('request')
-const fs = require('fs')
-const async =require('asyncs')
-
- 
+const express = require('express');
+const app = express();
+const port = 3001;
+//路由配置
+const routes = require('./Routes/rotue');
+//加载入口页面
+routes.Home(app);
+//设置静态文件
+app.use(express.static('./Public'));
+var server = app.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(server.address());
+    console.log('listening at http://%s:%s', host, port);
+});
